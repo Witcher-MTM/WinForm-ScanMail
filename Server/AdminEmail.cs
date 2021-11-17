4d71048c3cf1146ca921416e48d23a1b67555e7f
Witcher-MTM <phantomit228@gmail.com>
2021-11-17 19:27:40 +0300

    Add project files.

---
+++ Server/AdminEmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public static class AdminEmail
+    {
+        public static string Email = "rubicktanks@gmail.com";
+        public static string Pass = "superpass2003";
+        
+    }
+}
